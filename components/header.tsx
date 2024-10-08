3f5cfcfac7dc10b3c0d770806a4af9da60d41852
mstout60 <mstout@eprospector.com>
2024-10-08 13:21:58 -0500

    Implement Next JS with Clerk auth

---
+++ components/header.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { PenBox } from 'lucide-react';
+import { SignedOut, SignedIn, SignInButton } from "@clerk/nextjs";
+import UserMenu from "@/components/user-menu";
+
+const Header = () => {
+    return (
+        <nav className="mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2">
+            <Link
+                href="/"
+                className="flex items-center"
+            >
+                <Image
+                    src='/logo.png'
+                    width={150}
+                    height={60}
+                    alt="Schedullr Logo"
+                    className="h-16 w-auto"
+                />
+            </Link>
+            <div className="flex items-center gap-4">
+                <Link
+                    href='/events>create=true'
+                >
+                    <Button className="flex items-center gap-2">
+                        <PenBox size={18} />
+                        Create Event
+                    </Button>
+                </Link>
+                <SignedOut>
+                    <SignInButton forceRedirectUrl="/dashboard">
+                        <Button variant="outline">
+                            Login
+                        </Button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                   <UserMenu />
+                </SignedIn>
+            </div>
+        </nav>
+    );
+}
+
+export default Header
